ab7293d105a30ad3597f676d26ce7225ac13e40c
Aleksei Chikin <mail@makingoff.name>
2021-11-05 21:04:38 +0300

    Initial Commit

---
+++ pipeline tracker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  pipeline tracker
+//
+//  Created by Aleksei Chikin on 05.11.2021.
+//  Copyright © 2021 Aleksei Chikin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
